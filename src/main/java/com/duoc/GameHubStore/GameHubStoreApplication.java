b7f561edb3eb60fa114698d21060f1b1015e4c9f
melafiori <cachilu@proton.me>
2026-05-13 10:15:25 -0400

    Init Commit

---
+++ src/main/java/com/duoc/GameHubStore/GameHubStoreApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.GameHubStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameHubStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameHubStoreApplication.class, args);
+	}
+
+}
