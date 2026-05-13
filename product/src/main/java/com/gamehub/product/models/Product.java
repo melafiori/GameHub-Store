fa48b74dc2f3b0fc8e5439c7333d3de6d2f05e17
melafiori <cachilu@proton.me>
2026-05-13 11:33:30 -0400

    Añadido clase Product y clase Audit en modelo de microservicio product

---
+++ product/src/main/java/com/gamehub/product/models/Product.java
@@ -0,0 +1,50 @@
+package com.gamehub.product.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name ="productos")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long productId;
+
+
+    @Column(name = "product_nombre", nullable = false)
+    @NotBlank(message = "El campo nombre del producto no puede ser vacío.")
+    private String nombre;
+
+    @Column(name = "product_marca", nullable = false)
+    @NotBlank(message = "El campo marca del producto no puede ser vacío.")
+    private String marca;
+
+    @Column(name ="product_modelo", nullable = false)
+    @NotBlank(message = "El campo modelo del producto no puede ser vacío.")
+    private String modelo;
+
+    @Column(name = "product_precio", nullable = false)
+    private double precio;
+
+    // ToDo
+    // private Long categoriaId;
+
+    @Column(name = "product_descripcion", nullable = false)
+    private String descripcion;
+
+    @Column(name = "product_estado", nullable = false)
+    private String estado;
+
+    @Embedded
+    private Audit audit = new Audit();
+}
