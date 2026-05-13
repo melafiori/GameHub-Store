9f4a60a3a5fa44bd4d3ab13490fcf42a40df4914
cvroro <caro.riverar@duocuc.cl>
2026-05-13 11:43:52 -0400

    Se añadió el model de microservicio user

---
+++ user/src/main/java/models/User.java
@@ -0,0 +1,39 @@
+package models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_nombre", nullable = false)
+    @NotBlank(message = "El campo nombre del usuario no puede estar vacío.")
+    private String nombre;
+
+    @Email
+    @Column(name = "user_email", nullable = false)
+    @NotBlank(message = "El campo email del usuario no puede estar vacío.")
+    private String email;
+
+    @Column(name = "user_telefono", nullable = false)
+    @NotBlank(message = "El campo telefono del usuario no puede estar vacío.")
+    private String telefono;
+
+    @Column(name = "user_rol", nullable = false)
+    @NotBlank(message = "El campo rol del usuario no puede estar vacío.")
+    private String rol;
+
+    @Column(name = "user_estado", nullable = false)
+    @NotBlank(message = "El campo estado del usuario no puede estar vacío.")
+    private String estado;
+}
