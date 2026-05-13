ba828ce0d20b1bcc5c8ed57d7cdd8491017e8f27
cvroro <caro.riverar@duocuc.cl>
2026-05-13 11:54:22 -0400

    Se añadió el Audit de microservicio user

---
+++ user/src/main/java/models/Audit.java
@@ -0,0 +1,30 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+
+public class Audit  {
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
