0744eb7d4e73b073926ea103b39f8f328c50b12a
cvroro <caro.riverar@duocuc.cl>
2026-06-02 12:02:25 -0400

    Agrego pom, swagger, properties a Payment y mini correcion en auth

--- payment/src/main/java/com/gamehub/payment/config/SwaggerConfig.java
+++ payment/src/main/java/com/gamehub/payment/config/SwaggerConfig.java
@@ -1,21 +1,19 @@
-package com.gamehub.auth.config;
+package com.gamehub.payment.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
     @Bean
-    public OpenAPI customOpenAPI(){
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API Auth")
+                        .title("API Payment")
                         .version("1.0")
-                        .description("Documentacion de la API de Auth")
+                        .description("Documentacion de la API de Payment")
                 );
     }
 }
