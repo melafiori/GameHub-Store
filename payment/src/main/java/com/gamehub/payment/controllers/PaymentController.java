36a91147080ec97840e1cfe74d2dd9deee0f210e
melafiori <cachilu@proton.me>
2026-06-20 19:38:14 -0400

    Anotaciones Swagger añadidas en PaymentController: mejoras en la documentación de endpoints, validaciones, cambios en rutas, y uso de ResponseEntity.

--- payment/src/main/java/com/gamehub/payment/controllers/PaymentController.java
+++ payment/src/main/java/com/gamehub/payment/controllers/PaymentController.java
@@ -2,22 +2,30 @@ package com.gamehub.payment.controllers;
 
 import com.gamehub.payment.models.Payment;
 import com.gamehub.payment.services.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/payments")
-@RequiredArgsConstructor
+@RequestMapping("/api/v1/payments")
+@Validated
+@Tag(name = "Payments V1", description = "Se encarga de procesar pagos.")
 public class PaymentController {
 
     @Autowired
     private PaymentService paymentService;
 
     @PostMapping
+    @Operation(summary = "Procesar un pago", description = "Procesa un pago y devuelve el resultado.")
+    @ApiResponse(responseCode = "200", description = "Pago procesado correctamente.")
     private ResponseEntity<?> procesarPago(@RequestBody Payment request) {
         return ResponseEntity.ok(paymentService.procesarPago(request));
     }
