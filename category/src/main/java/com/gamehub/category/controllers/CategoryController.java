aa7851a905aeeeaeb3558e4d1ef1989aed5ea210
melafiori <cachilu@proton.me>
2026-06-20 20:06:55 -0400

    Anotaciones Swagger añadidas en CategoryController: mejoras en la documentación de endpoints, validaciones, uso de ResponseEntity, y descripción de rutas.

--- category/src/main/java/com/gamehub/category/controllers/CategoryController.java
+++ category/src/main/java/com/gamehub/category/controllers/CategoryController.java
@@ -2,6 +2,13 @@ package com.gamehub.category.controllers;
 
 import com.gamehub.category.models.Category;
 import com.gamehub.category.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,38 +22,75 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/categories")
 @Validated
+@Tag(name = "Categories V1", description = "Metodos CRUD para la gestión de categorías")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
     @GetMapping
+    @Operation(
+            summary = "Listado de todas las categorías",
+            description = "Se devuelve una lista con las categorías encontradas en la BdD"
+    )
+    @ApiResponse(responseCode = "200", description = "Operación exitosa.")
     public ResponseEntity<List<Category>> getAll() {
         List<Category> categorias = this.categoryService.findAll();
         return ResponseEntity.ok(categorias);
     }
 
-
+    @Operation(
+            summary = "Busqueda de una categoría",
+            description = "Devuelve una categoría, o en caso contrario, una excepción"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Categoría encontrada",
+                    content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Category.class))
+            ),
+            @ApiResponse(responseCode = "404", description = "Categoría no encontrada.")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Category> getById(@PathVariable Long id) {
+    public ResponseEntity<Category> getById(
+            @Parameter(description = "Id de categoría a buscar")
+            @PathVariable Long id) {
         Category categoria = this.categoryService.findById(id);
         return ResponseEntity.ok(categoria);
     }
 
     @PostMapping
+    @Operation(summary = "Guardado de categoría", description = "Esta es la forma de guardar una categoría nueva.")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Categoría a guardar", required = true,
+            content =  @Content(schema = @Schema(implementation = Category.class))
+    )
     public ResponseEntity<Category> create(@Valid @RequestBody Category category) {
         Category nuevaCategoria = this.categoryService.save(category);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaCategoria);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Category> update(@PathVariable Long id, @Valid @RequestBody Category category) {
+    @Operation(summary = "Actualización de categoría", description = "Se actualizan los datos de una categoría en la BdD.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoría encontrada."),
+            @ApiResponse(responseCode = "404", description = "Categoría no encontrada.")
+    })
+    public ResponseEntity<Category> update(
+            @Parameter(description = "Id de la categoría a actualizar", required = true, example = "1")
+            @PathVariable Long id,
+            @Valid @RequestBody Category category) {
         Category categoriaActualizada = this.categoryService.updateById(id, category);
         return ResponseEntity.ok(categoriaActualizada);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
+    @Operation(summary = "Eliminación de una categoría", description = "Se elimina una categoría de la BdD")
+    @ApiResponse(responseCode = "204", description = "Categoría eliminada.")
+    public ResponseEntity<Void> delete(
+            @Parameter(description = "Id de la categoría a eliminar", required = true, example = "1")
+            @PathVariable Long id) {
         this.categoryService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
